d39e5924073c9f5ed1f7ff97260d6b5058c3c646
xPyXer <MateusLeoSilva666@gmail.com>
2021-10-15 17:40:28 -0300

    first-commit

---
+++ UC7-senai-versoes-colaboradores-master/README.md
@@ -0,0 +1,6 @@
+# senai-versoes-colaboradores
+
+README de exemplo
+
+Olá, eu passei por aqui! 
+Não se esqueça do comando: "git commit" e do "git status"
---
+++ UC8-senai-master/sistema-de-cadastro-de-eventos/index.js
@@ -0,0 +1,33 @@
+// Verifica se a data é posterior a "7/09/2021"
+var data = "hoje"
+console.log(data)
+if(data < "hoje") {
+    console.log('Data inválida, não foi possível fazer o cadastro')
+};
+
+// Abre um prompt para inserir sua idade
+var idade = Number(prompt("Digite sua idade: "));
+console.log(idade);
+
+// Verifica a idade inserida
+if(idade >= 18) {
+    console.log('Acesso concedido, prossiga com o cadastro!')
+} else {
+    console.log('Acesso negado! Você precisa ter mais que 18 anos.')
+};
+
+
+if participantes = 0 {
+    participantes = false
+} else {
+    participantes = true
+}
+
+console.log(participantes)
+// Se tiver menos de 100 cadastrados é possível cadastrar, caso contrário não será possível
+while(participantes < 100) {
+    cadastar === true;
+} if (participantes > 100) {
+    alert("Cadastro recusado! Limite excedido!")
+    cadastar === false;
+};
